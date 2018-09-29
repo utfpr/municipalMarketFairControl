9ec459472648717d6b766985ca76d84dd6165fb8
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2018-09-29 18:01:42 -0300

    teste de relacoes para categoria pronto

--- backend/tests/models/categoria.js
+++ backend/tests/models/categoria.js
@@ -1,55 +1,63 @@
-// const chance = require("chance").Chance();
-// const { assert } = require("chai");
+const chance = require("chance").Chance();
+const { assert } = require("chai");
 
-// const models = require("../../models");
+const models = require("../../models");
 
-// after(() => {
-//   models.sequelize.close();
-// });
+after(() => {
+  models.sequelize.close();
+});
 
-// describe("Testando categoria", () => {
-//   beforeEach(() => {
-//     models.feirante.destroy({ where: {} });
-//     models.categoria.destroy({ where: {} });
-//     models.subcategoria.destroy({ where: {} });
-//     models.feira.destroy({ where: {} });
-//     models.celula.destroy({ where: {} });
-//     models.participa.destroy({ where: {} });
-//   });
+describe("Testando categoria", () => {
+  beforeEach(() => {
+    models.feirante.destroy({ where: {} });
+    models.categoria.destroy({ where: {} });
+    models.subcategoria.destroy({ where: {} });
+    models.feira.destroy({ where: {} });
+    models.celula.destroy({ where: {} });
+    models.participa.destroy({ where: {} });
+    models.supervisor.destroy({ where: {} });
+  });
 
-//   it("Adiciona categoria", async () => {
-//     let categoria = await models.categoria.findOne({
-//       where: {
-//         id: "",
-//         nome: "alimentos",
-//         need_cnpj: false
-//       }
-//     });
-//     assert.isNull(categoria);
+  it("Adiciona categoria", async () => {
+    let categoria = await models.categoria.findOne({
+      where: {
+        id: "",
+        nome: "alimentos",
+        need_cnpj: false
+      }
+    });
+    assert.isNull(categoria);
 
-//     categoria = await models.categoria.create({
-//       id: "",
-//       nome: "alimentos",
-//       need_cnpj: false
-//     });
-//     categoria = await models.categoria.findOne({
-//       where: {
-//         nome: "alimentos",
-//         need_cnpj: false
-//       }
-//     });
-//     assert.isNotNull(categoria);
-//     assert.strictEqual(categoria.nome, "alimentos");
-//   });
+    categoria = await models.categoria.create({
+      id: "",
+      nome: "alimentos",
+      need_cnpj: false
+    });
+    categoria = await models.categoria.findOne({
+      where: {
+        nome: "alimentos",
+        need_cnpj: false
+      }
+    });
+    assert.isNotNull(categoria);
+    assert.strictEqual(categoria.nome, "alimentos");
+  });
 
-//   it("Listar subcategorias de categoria", async () => {
-//     let categoria = await models.categoria.create({
-//       id: "",
-//       nome: "alimentos",
-//       need_cnpj: false
-//     });
+  it("Listar subcategorias de categoria", async () => {
+    const categoria = await models.categoria.create({
+      id: 1,
+      nome: "alimentos",
+      need_cnpj: false
+    });
 
-//     const subcategoria = await categoria.getSubCategorias();
-//     assert.lengthOf(subcategoria, 0);
-//   });
-// });
+    const subcategoria = await models.subcategoria.create({
+      id: 1,
+      nome: "pastel do tio",
+      categoria_id: 1
+    });
+
+    assert.lengthOf(await categoria.getSubCategorias(), 0);
+    await categoria.addSubCategorias(subcategoria);
+    assert.lengthOf(await categoria.getSubCategorias(), 1);
+  });
+});
