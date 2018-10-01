b7b20f052af66c4d0c12dfeaf3819bdb24721504
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2018-10-01 18:46:38 -0300

    models de celula atualizado

--- backend/models/celula.js
+++ backend/models/celula.js
@@ -3,46 +3,38 @@
 
 module.exports = function(sequelize, DataTypes) {
   const Celula = sequelize.define(
-    'celula',
+    "celula",
     {
       id: {
         type: DataTypes.INTEGER(11),
         allowNull: false,
-        defaultValue: '0',
-        primaryKey: true,
+        defaultValue: "0",
+        primaryKey: true
       },
       cpf_feirante: {
         type: DataTypes.STRING(15),
         allowNull: true,
         references: {
-          model: 'feirante',
-          key: 'cpf',
-        },
+          model: "feirante",
+          key: "cpf"
+        }
       },
       periodo: {
         type: DataTypes.INTEGER(11),
-        allowNull: false,
-      },
-      participa_cpf: {
-        type: DataTypes.STRING(15),
-        allowNull: true,
-      },
-      participa_data: {
-        type: DataTypes.DATEONLY,
-        allowNull: true,
-      },
+        allowNull: false
+      }
     },
     {
-      tableName: 'celula',
+      tableName: "celula",
       timestamps: false,
-      createdAt: false,
-    },
+      createdAt: false
+    }
   );
   Celula.associate = models => {
     models.celula.hasMany(models.participa, {
-      foreignKey: 'celula_id',
-      sourceKey: 'id',
-      as: 'CelulasParticipa',
+      foreignKey: "celula_id",
+      sourceKey: "id",
+      as: "CelulasParticipa"
     });
   };
   return Celula;
