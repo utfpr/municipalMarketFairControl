a572af128057d26b2d01a5f61918193c03e2d38c
ogoes17 <otaviosilva820@gmail.com>
2018-10-04 14:29:56 -0300

    routes/login -- inicial

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,24 @@
-const express = require('express');
+/* eslint-disable */
 
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+var login = require('./routes/login');
+
+app.get('/', (req, res) => {
+    console.log("logadaço");
+});
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use('/login', login);
+
+
 
 module.exports = app;
