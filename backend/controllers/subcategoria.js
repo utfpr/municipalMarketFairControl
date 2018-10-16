d8c8d71148b6773bf530188535a68e578572cfd9
danielbom <daniel.rodrigues.45@hotmail.com>
2018-10-16 12:06:26 -0300

    setSubcategoria implementado. Alguns comentários adicionados. Falta teste

--- backend/controllers/subcategoria.js
+++ backend/controllers/subcategoria.js
@@ -1,6 +1,8 @@
 const models = require('../models');
 
 const addSubcategoria = async (nome, categoria_id) => {
+  // O banco de dados não pode fazer essas verificações para mim?
+
   // Verificando se existe alguma categoria com esse id
   const categoria = await models.categoria.findOne({
     where: {
@@ -24,7 +26,7 @@ const addSubcategoria = async (nome, categoria_id) => {
 
   // Adicionando subcategoria
   try {
-    await models.subcategoria.create({
+    return await models.subcategoria.create({
       nome: nome,
       categoria_id: categoria_id,
     });
@@ -33,59 +35,106 @@ const addSubcategoria = async (nome, categoria_id) => {
   }
 };
 
-const listSubcategoria = async () => {
-  const subcategorias = await models.supervisor.findAll({
+const listSubcategorias = async () => {
+  // Retorna todas as subcategorias, de todas as categorias
+  // Se não houver nenhuma, retorna um array vazio
+  const subcategorias = await models.subcategoria.findAll({
     where : {}
   });
 
   return subcategorias.map(el => ({
+    id: el.id,
     nome: el.nome,
     categoria_id: el.categoria_id
   }));
 };
 
-const getSubcategoria = async (nome) => {
+const listSubcategoriasByCategoria = async (nome_categoria) => {
+  // Retorna todas as subcategorias de uma categoria
+  // Se não existir a categoria ou nao existir subcategorias dessa categoria,
+  // retorna um array vazio
+
+  // Buscando a categoria
+  const categoria = await models.categoria.findOne({
+    where: {nome: nome_categoria}
+  });
+  if (categoria === NULL)
+    return {};
+
+  // Buscando as subcategorias da categoria
+  const subcategorias = await models.subcategoria.findAll({
+    where: {categoria_id: categoria.id}
+  });
+
+  return subcategorias.map(el => ({
+    id: el.id,
+    nome: el.nome,
+    categoria_id: el.categoria_id,
+  }));
+}
+
+const getSubcategoria = async (id) => {
+  // Por que eu buscaria uma subcategoria em específico?
+
+  return await models.subcategoria.findOne({
+    where: { id: id },
+  })
+
+  /*
   const subcategoria = await models.subcategoria.findOne({
-    where: {
-      nome: nome
-    }
+    where: { nome: nome },
   });
 
   if (subcategoria === null)
-  return null;
+    return null;
 
   return {
     id: subcategoria.id,
     nome: subcategoria.nome,
     categoria_id: subcategoria.categoria_id
   };
+  */
 }
 
-// TODO : setSubcategoria
-const setSubcategoria = async (nome, novo_nome) => {
-
+const setSubcategoria = async (id, novo_nome) => {
+  const subcategoria = getSubcategoria(id);
+  
+  if(subcategoria === null)
+    return null;
+  
+  // Por que usar try?
+  try {
+    const res = await subcategoria.update({
+      nome: novo_nome
+    });
+    return res;
+  } catch (error) {
+    return null;
+  }
 }
 
-const deleteSubcategoria = async (nome) => {
-  // Verificando se existe uma subcategoria com esse nome
+const deleteSubcategoria = async (id) => {
+  // Verificando se existe uma subcategoria com esse id
   const subcategoria = await models.subcategoria.findOne({
-    where: {
-      nome: nome
-    }
+    where: { id: id }
   });
 
+  // Retorna false caso nao consiga?
   if (subcategoria === null)
-    return null;
+    return false;
 
   // Removendo subcategoria
   await models.subcategoria.destroy({
-    where: {nome : subcategoria.nome}
+    where: {id : subcategoria.id}
   });
+
+  return true;
 };
 
 module.exports = {
   addSubcategoria,
-  listSubcategoria,
+  listSubcategorias,
+  listSubcategoriasByCategoria,
   getSubcategoria,
   setSubcategoria,
   deleteSubcategoria,
