0230584f812d093372c9f3226cf333f02f66b6d8
ogoes17 <otaviosilva820@gmail.com>
2018-10-16 18:52:46 -0300

    ant-design-vue

---
+++ frontend/src/ant-design.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import 'ant-design-vue/dist/antd.css';
+
+const Ant = require('ant-design-vue');
+
+// alguns exemplos
+Vue.use(Ant.Tag);
+Vue.use(Ant.Button);
+Vue.use(Ant.Checkbox);
+Vue.use(Ant.Menu);
+Vue.use(Ant.Table);
+Vue.use(Ant.Input);
