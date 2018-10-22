f459bea194d3fb8b617cd0051ae01adb5cf50e9e
Jorge Rossi <jorgefzrossi@gmail.com>
2018-10-21 23:25:56 -0300

    Tentando fazer CRUD supervisor

---
+++ frontend/src/components/supervisor/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input placeholder="CPF" v-model="model.cpf" :disabled="this.action === 'view'" />
+    <input type="text" v-model="model.nome" :disabled="this.action === 'view'" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'modal',
+  props: {
+    action: {
+      type: String
+    },
+    model: {
+      type: Object
+    }
+  },
+
+  watch: {
+    model: {
+      handler: function (newValue) {
+        this.$emit('updateModel', newValue);
+      },
+      deep: true
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/CrudSupervisor.vue
@@ -0,0 +1,33 @@
+<template>
+  <a-layout id="content">
+    <div id="table-container">
+      <Table />
+    </div>
+  </a-layout>
+</template>
+
+<script>
+
+import Table from '@/components/supervisor/Table.vue';
+
+
+export default {
+  components: {
+    Table,
+  },
+}
+</script>
+
+
+<style scoped>
+#content {
+  display: flex;
+  flex-direction: column;
+}
+
+#table-container {
+  flex: 1;
+  padding: 15px;
+  overflow: hidden;
+}
+</style>
