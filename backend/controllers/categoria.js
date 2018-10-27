7d9b926ef77e7a93dba1d16886c68bae39ff73ee
LucasHenriqueP <lucashenrique.pereira@hotmail.com>
2018-10-26 21:32:07 -0300

    Controller Categoria terminado

--- backend/controllers/categoria.js
+++ backend/controllers/categoria.js
@@ -4,12 +4,17 @@ const addCategoria = async(nome, need_cnpj) => {
     const categoria = await models.categoria.findOne({
         where:{ nome }
     });
-    if(categoria != null) return null;
+    if(categoria != null) {
+        console.log("Categoria já cadastrada");
+        return null;
+    }
     try {
-        await models.categoria.create({
+        let res = await models.categoria.create({
             nome, need_cnpj
         });
+        return res;
     } catch(error){
+        console.log(`Erro ao cadastrar`);
         return null
     }
 };
@@ -20,22 +25,17 @@ const listCategorias = async() =>{
         id: el.id,
         nome: el.nome,
         need_cnpj: el.need_cnpj
-    }))
+    }));
 }
 
-const findByNome = async(nome) =>{
-    const categoria = await models.categoria.findOne({
-        where: { nome }
-    });
-    if(categoria == null) return null;
-    return {id: categoria.id, nome: categoria.nome, need_cnpj: categoria.need_cnpj}
-}
 
-const findById = async(id) =>{
+const findByid = async(id) =>{
     const categoria = await models.categoria.findOne({
         where: { id }
     });
-    if(categoria == null) return null;
+    if(categoria == null){
+        return null;
+    }
     return {id: categoria.id, nome: categoria.nome, need_cnpj: categoria.need_cnpj}
 }
 
@@ -43,16 +43,26 @@ const removeCategoria = async(id) =>{
     const categoria = await models.categoria.findOne({
         where: { id }
     });
-    if (categoria = null) return null;
+    if (categoria == null) return null;
     models.categoria.destroy({
         where:{ id }
-    })
+    });
+}
+
+const updateCategoria = async(id, dados) =>{
+    const categoria = await models.categoria.findOne({
+        where: { id }
+    });
+    if (categoria == null) {
+        return null;
+    }
+    await categoria.update(dados);
 }
 
 module.exports = {
     addCategoria,
     listCategorias,
-    findById,
-    findByNome,
-    removeCategoria
+    findByid,
+    removeCategoria,
+    updateCategoria
 }
--- backend/tests/controllers/categoria.js
+++ backend/tests/controllers/categoria.js
@@ -13,23 +13,52 @@ after(() => {
       });
 
   it('Cadastrar categorias', async()=>{
-      let res = await categoriaController.addCategoria("Salgadinhos", true);
+      models.categoria.destroy({ where: {} });
+      let res = await categoriaController.addCategoria('Salgadinhos', true);
+      //console.log(res.dataValues.id);
       assert.isNotNull(res);
 
   });
-  it('Achar por nome', async()=>{
-    let res = await categoriaController.addCategoria("Salgadinhos", true);
-    assert.isNotNull(res);
-    categoria = await categoriaController.findByNome("Salgadinhos");
-    assert.isNotNull(categoria);
-    console.log(categoria);
-  });
+ 
   it('Achar por id', async()=>{
-    let res = await categoriaController.addCategoria("Salgadinhos", true);
+    await models.categoria.destroy({ where: { } });
+    let res = await categoriaController.addCategoria('Pastel', true);
     assert.isNotNull(res);
-    categoria = await categoriaController.findByid(14);
+    let categoria = await categoriaController.findByid(res.dataValues.id);
     assert.isNotNull(categoria);
-    console.log(categoria);
+  });
+
+  it('Remover por Id', async()=>{
+    let categoria = await categoriaController.addCategoria('Verduras', true);
+    assert.isNotNull(categoria);
+    let res = await categoriaController.removeCategoria(categoria.dataValues.id);
+    assert.isNotNull(res);
+    res = await categoriaController.findByid(categoria.dataValues.id);
+    assert.isNull(res);
+  });
+
+  it('Atualizar dados', async()=>{
+    let categoria = await categoriaController.addCategoria('Caldo de cana', true);
+    assert.isNotNull(categoria);
+    let res = await categoriaController.updateCategoria(categoria.dataValues.id, {need_cnpj: false});
+    assert.isNotNull(res);
+    let res1 = await categoriaController.updateCategoria(categoria.dataValues.id, {nome: "Artesanato", need_cnpj: false});
+    assert.isNotNull(res1);
+    console.log(categoria.dataValues);
+    let novo = await categoriaController.findByid(categoria.dataValues.id);
+    console.log(novo);
+  });
+
+  it('Listar todas Categorias', async()=>{
+    let categoria = await categoriaController.addCategoria('Caldo de cana', true);
+    assert.isNotNull(categoria);
+    categoria = await categoriaController.addCategoria('Verduras', true);
+    assert.isNotNull(categoria);
+    let res = await categoriaController.addCategoria('Pastel', true);
+    assert.isNotNull(res);
+
+    let categorias = await categoriaController.listCategorias();
+    console.log(categorias);
   })
 
 });
