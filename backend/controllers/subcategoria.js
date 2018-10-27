393701c41c437411f162d8e887dfadb4f00f2dbc
danielbom <daniel.rodrigues.45@hotmail.com>
2018-10-27 18:38:45 -0300

    Reimplementando apenas as funções necessárias para subcategoria

--- backend/controllers/subcategoria.js
+++ backend/controllers/subcategoria.js
@@ -1,146 +1,51 @@
 const models = require('../models');
-//const controllerCategoria = require('categoria');
-//OKAY
-const exist = (obj) => {
-  return obj !== null;
-}
-// OKAY
-const notExist = (obj) => {
-  return obj === null;
-}
-// OKAY
-const getCategoriaByNome = async (nome) => {
-  return models.categoria.findOne({
-    where: { nome: nome }
-  });
-}
-// OKAY
-const getCategoriaById = async (id) => {
-  return models.categoria.findOne({
-    where: { id: id }
-  });
-}
 
 
-/* ******************************************************************** */
-/*             Funções acima não devem estar neste arquivo              */
-/* ******************************************************************** */
-
-// OKAY
-const getSubcategoria = async (nome, categoria_id) => {
+const findSubcategoriaById = async (subcategoria_id) => {
   return models.subcategoria.findOne({
-    where: { nome: nome, categoria_id: categoria_id }
+    where: { id: subcategoria_id }
   });
 }
 
-const findSubcategoriaById = async (id) => {
-  return models.subcategoria.findOne({
-    where: { id: id }
-  });
-}
-
-const setSubcategoria = async (nome, categoria_id, novo_nome) => {
-  // Verificando se existe a categoria
-  const categoria = await getCategoriaById(categoria_id);
-
-  // Se a categoria não existir, não é possível fazer a atualização
-  if (notExist(categoria))
-    return null;
-
-  // Verificando se já existe essa subcategoria
-  const subcategoria = await getSubcategoria(nome, categoria.id);
-
-  // Se a subcategoria não existir, não é possível fazer a atualização
-  if (notExist(subcategoria))
-    return null;
-
-  // Caso contrario, atualiza
-  models.subcategoria.update(
-    { nome: novo_nome},
-    { where: {nome: nome, categoria_id: categoria.id} }
-  );
-}
-// OKAY
 const addSubcategoria = async (nome, categoria_id) => {
-  // Verificando se existe a categoria
-  const categoria = await getCategoriaById(categoria_id);
-
-  // Se a categoria não existir, não é possível adicionar
-  if (notExist(categoria))
-    return null;
-
-  // Verificando se já existe essa subcategoria
-  const subcategoria = await getSubcategoria(nome, categoria.id);
-
-  // Se já existe, não é preciso adicionar
-  if (exist(subcategoria))
-    return null;
-
-  // Caso contrario, adiciona
   try {
-    // Tentando adicionar e retornar uma subcategoria nova
     return models.subcategoria.create({
       nome: nome,
-      categoria_id: categoria.id,
+      categoria_id: categoria_id,
     });
   } catch (error) {
-    // Se acontecer algum erro retorna null
+    console.log(error);
     return null;
   }
-};
+}
 
-const deleteSubcategoria = (nome, categoria_id) => {
-  // Verificando se existe a categoria
-  const categoria = getCategoriaById(categoria_id);
-
-  // Se a categoria não existir, não é possível fazer a atualização
-  if (notExist(categoria))
-    return null;
-
-  // Verificando se já existe essa subcategoria
-  const subcategoria = getSubcategoria(nome, categoria.id);
-
-  // Se a subcategoria não existir, não é possível fazer a atualização
-  if (notExist(subcategoria))
-    return null;
-
-  // Caso contrário, deleta
+const updateSubcategoria = async (subcategoria_id, novo_nome) => {
   try {
-    // Removendo subcategoria
-    subcategoria.destroy();
+    models.subcategoria.update(
+      { nome: novo_nome },
+      { where: { id: subcategoria_id } }
+    );
   } catch (error) {
+    console.log(error);
     return null;
   }
-};
-// OKAY
-const listSubcategorias = async () => {
-  // Retorna todas as subcategorias, de todas as categorias
-  // Se não houver nenhuma, retorna um array vazio
-  return models.subcategoria.findAll({
-    where: {}
-  });
-};
-// OKAY
-const listSubcategoriasByCategoria = async (categoria_id) => {
-  // Verificando se a categoria existe
-  const categoria = await getCategoriaById(categoria_id);
+}
 
-  // Se a categoria não existir, não é possível listar as suas subcategorias
-  if (notExist(categoria))
+const deleteSubcategoria = async (subcategoria_id) => {
+  try {
+    models.subcategoria.destroy({ where: { id: subcategoria_id } });
+  } catch (error) {
+    console.log(error);
     return null;
-
-  // Caso contrário, retorna uma lista das subcategorias da categoria
-  return await models.subcategoria.findAll({
-    where: { categoria_id: categoria.id }
-  });
+  }
 }
 
 module.exports = {
-  getSubcategoria, setSubcategoria,
-
   findSubcategoriaById,
 
-  addSubcategoria, deleteSubcategoria,
+  addSubcategoria,
 
-  listSubcategorias, listSubcategoriasByCategoria,
+  updateSubcategoria,
+
+  deleteSubcategoria,
 }
--- backend/tests/controllers/subcategoria.js
+++ backend/tests/controllers/subcategoria.js
@@ -8,8 +8,26 @@ describe('Controller subcategoria', () => {
     await models.subcategoria.destroy({ where: {} });
   });
 
+  describe('findSubcategoriaById', () => {
+    it('Retorna null caso não encontrar', async () => {
+      // Recuperando um subcategoria inexistente
+      const subcategoria_null = await subcategoriaController.findSubcategoriaById(9999999999999999999999999999);
+      assert.isNull(subcategoria_null);
+    });
+
+    it('Retorna um ponteiro no caso de encontrar', async () => {
+      // Criando uma categoria e uma subcategoria na mão
+      const categoria = await models.categoria.create({ nome: 'Categoria_1', need_cnpj: false });
+      const subcategoria = await models.subcategoria.create({ nome: 'Subcategoria_11', categoria_id: categoria.id });
+
+      // Recuperando a subcategoria criada
+      const subcategoria_not_null = await subcategoriaController.findSubcategoriaById(subcategoria.id);
+      assert.isNotNull(subcategoria_not_null);
+    });
+  });
+
   describe('addSubcategoria', () => {
-    it('Retorna null se não for possível criar. Caso contrário, retorna um ponteiro para a subcategoria criada.', async () => {
+    it('Retorna um ponteiro para a subcategoria criada.', async () => {
       // Criando na mão uma categoria para teste
       const categoria = await models.categoria.create({ nome: 'Categoria_1', need_cnpj: false });
       assert.isNotNull(categoria);
@@ -18,114 +36,34 @@ describe('Controller subcategoria', () => {
       const subcategoria = await subcategoriaController.addSubcategoria("Subcategoria_11", categoria.id);
       assert.isNotNull(subcategoria);
 
-      // Tentando criar a mesma subcategoria criada para teste
-      const subcategoria2 = await subcategoriaController.addSubcategoria("Subcategoria_11", categoria.id);
-      assert.isNull(subcategoria2);
+      // OBS: É possível adicionar 2 subcategoria com o mesmo nome
     });
   });
 
-  describe('listSubcategoria', () => {
-    it('Retorna array vazio quando não existe subcategorias', async () => {
-      // Recuperando as subcategorias sem existir nenhuma
-      const subcategorias = await subcategoriaController.listSubcategorias();
-      assert.lengthOf(subcategorias, 0);
-    });
-
-    it('Retorna um array de todas as subcategorias', async () => {
-      // Criando na mão duas categorias para teste
-      const categoria1 = await models.categoria.create({ nome: 'Categoria_1', need_cnpj: false });
-      const categoria2 = await models.categoria.create({ nome: 'Categoria_2', need_cnpj: false });
-
-      // Criando na mão subcategorias para as categorias de teste
-      await models.subcategoria.create({ nome: 'Subcategoria_11', categoria_id: categoria1.id });
-      await models.subcategoria.create({ nome: 'Subcategoria_12', categoria_id: categoria1.id });
-      await models.subcategoria.create({ nome: 'Subcategoria_21', categoria_id: categoria2.id });
-      await models.subcategoria.create({ nome: 'Subcategoria_22', categoria_id: categoria2.id });
-
-      // Listando todas as categorias existentes
-      const subcategorias = await subcategoriaController.listSubcategorias();
-      assert.lengthOf(subcategorias, 4);
-    });
-  });
-
-  describe('listSubcategoriaByCategoria', () => {
-    it('Retorna null quando não existe subcategorias.', async () => {
-      // Recuperando a subcategorias de uma categoria inexistente
-      const subcategorias_null = await subcategoriaController.listSubcategoriasByCategoria("Categoria_xxx");
-      assert.isNull(subcategorias_null);
-    });
-
-    it('Retorna um array de todas as subcategorias.', async () => {
-      // Criando na mão duas categorias para teste
-      const categoria1 = await models.categoria.create({ nome: 'Categoria_1', need_cnpj: false });
-      const categoria2 = await models.categoria.create({ nome: 'Categoria_2', need_cnpj: false });
-
-      // Criando na mão subcategorias para as categorias de teste
-      await models.subcategoria.create({ nome: 'Subcategoria_11', categoria_id: categoria1.id });
-      await models.subcategoria.create({ nome: 'Subcategoria_12', categoria_id: categoria1.id });
-
-      await models.subcategoria.create({ nome: 'Subcategoria_21', categoria_id: categoria2.id });
-
-      // Listando as subcategorias da Categoria_1
-      const subcategorias1 = await subcategoriaController.listSubcategoriasByCategoria(categoria1.id);
-      assert.lengthOf(subcategorias1, 2);
-
-      // Listando as subcategorias da Categoria_2
-      const subcategorias2 = await subcategoriaController.listSubcategoriasByCategoria(categoria2.id);
-      assert.lengthOf(subcategorias2, 1);
-    });
-  });
-
-  describe('findSubcategoriaById', () => {
-
-  });
-
-  describe('getSubcategoria', () => {
-    it('Retorna null caso não encontrar', async () => {
-      // Recuperando um subcategoria inexistente
-      const subcategoria_null = await subcategoriaController.getSubcategoria("SUBCATEGORIA");
-      assert.isNull(subcategoria_null);
-    });
-
-    it('Retorna um ponteiro no caso de encontrar', async () => {
-      // Criando uma categoria e uma subcategoria na mão
-      const categoria = await models.categoria.create({ nome: 'Categoria_1', need_cnpj: false });
-      await models.subcategoria.create({ nome: 'Subcategoria_11', categoria_id: categoria.id });
-
-      // Recuperando a subcategoria criada
-      const subcategoria2 = await subcategoriaController.getSubcategoria('Subcategoria_11', categoria.id);
-      assert.isNotNull(subcategoria2);
-    });
-  });
-
-  describe('setSubcategoria', () => {
+  describe('updateSubcategoria', () => {
     it('Alterando um nome de uma subcategoria', async () => {
       // Criando uma categoria e uma subcategoria na mão
       const categoria = await models.categoria.create({ nome: 'Categoria_1', need_cnpj: false });
-      await models.subcategoria.create({ nome: 'Subcategoria_11', categoria_id: categoria.id });
+      var subcategoria = await models.subcategoria.create({ nome: 'Subcategoria_11', categoria_id: categoria.id });
 
       // Alterando a subcategoria
-      await subcategoriaController.setSubcategoria('Subcategoria_11', categoria.id, "SUBCATEGORIA");
-
-      const subcategoria_null = await subcategoriaController.getSubcategoria('Subcategoria_11', categoria.id);
-      assert.isNull(subcategoria_null);
-
-      const subcategoria_not_null = await subcategoriaController.getSubcategoria('SUBCATEGORIA', categoria.id);
-      //assert.isNotNull(subcategoria_not_null); // Esta dando erro aki
+      await subcategoriaController.updateSubcategoria(subcategoria.id, "SUBCATEGORIA");
+      assert.equal(subcategoria.nome, "SUBCATEGORIA"); // Não altera...
     });
   });
 
+
   describe('deleteSubcategoria', () => {
-    it('Removendo uma subcategoria pelo nome', async () => {
+    it('Removendo uma subcategoria', async () => {
       // Criando uma categoria e uma subcategoria na mão
       const categoria = await models.categoria.create({ nome: 'Categoria_1', need_cnpj: false });
-      await models.subcategoria.create({ nome: 'Subcategoria_11', categoria_id: categoria.id });
+      const subcategoria = await models.subcategoria.create({ nome: 'Subcategoria_11', categoria_id: categoria.id });
 
       // Removendo a subcategoria
-      await subcategoriaController.deleteSubcategoria('Subcategoria_11', categoria.id);
+      await subcategoriaController.deleteSubcategoria(subcategoria.id);
 
-      const subcategoria_null = await subcategoriaController.getSubcategoria('Subcategoria_11', categoria.id);
-      assert.isNull(subcategoria_null); // Esta dando erro aki
+      const subcategoria_null = await subcategoriaController.findSubcategoriaById(subcategoria.id);
+      assert.isNull(subcategoria_null);
     });
   });
 });
