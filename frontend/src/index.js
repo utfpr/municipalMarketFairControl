5419d4ab94ab202a05765e06ae0749b798727cc2
Willian Rodrigues <oredstonebr@gmail.com>
2019-09-13 16:26:41 -0300

    Solicita cors para o backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
 import * as serviceWorker from './serviceWorker';
+
+axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
