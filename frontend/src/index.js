a3c626397d635f5f9c8cb607223ed49a9ff1eed8
Willian Rodrigues Barbosa <willianbarbosa@alunos.utfpr.edu.br>
2019-09-17 17:20:30 -0300

    Altera cabeçalho da requisição

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import * as serviceWorker from './serviceWorker';
 
 axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'https://deploy-preview-126--feiracriativacm.netlify.com';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
